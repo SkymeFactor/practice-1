1fb1e93e2d26598c193b3753d36b738089a704c6
Ma-Mush <alya.hahaha.ivanova@mail.ru>
2025-09-18 20:29:15 +0300

    logic changes

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
 int main(int argc, char** argv) {
   if (argc == 5) {
-    int wordindex = 0, fileindex = 0;
-    for (int i = 0; i < argc; i++) {
-      if (strcmp(argv[i], "--word") == 0) {
-        wordindex = i + 1;
-      } else if (strcmp(argv[i], "--file") == 0) {
-        fileindex = i + 1;
+    char* word = nullptr;
+    char* filename = nullptr;
+    for (int i = 1; i < argc; i++) {
+      if (strcmp(argv[i - 1], "--word") == 0) {
+        word = argv[i];
+      } else if (strcmp(argv[i - 1], "--file") == 0) {
+        filename = argv[i];
       }
     }
-    if (wordindex == 0 || fileindex == 0) {
+    if (word == nullptr || filename == nullptr) {
       std::cout << "Syntax Error: ./neatcounter --word <word> --file <file>";
       return 0;
     }
     
-    freopen(argv[fileindex], "r", stdin);
+    std::ifstream filein{filename};
     char now_word[100]; 
     int count = 0;
-    while (std::cin >> now_word) {
-      for (int i = 0; i < strlen(argv[wordindex]); i++) {
-        if (strrchr(now_word, (int) argv[wordindex][i]) == nullptr) {
+    while (filein >> now_word) {
+      for (int i = 0; i < strlen(word); i++) {
+        if (strrchr(now_word, (int) word[i]) == nullptr) {
           count--;
           break;
         }
