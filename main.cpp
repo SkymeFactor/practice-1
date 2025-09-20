8ff260c9a74a33b7cb78149b550311e5bf41a83a
Gosha Pogrebitskiy <goshik200712@gmail.com>
2025-09-20 16:07:51 +0300

    added argument parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello world\n";
+  bool nextIsFileName = false;
+  bool nextIsWord = false;
+  
+  std::string fileName = "";
+  std::string word = "";
+
+  for (int i = 1; i < argc; i++) { 
+    std::string Arg = argv[i];
+
+    if (nextIsFileName) {
+      fileName = Arg;
+      nextIsFileName = false;
+      continue;
+    }
+
+    if (nextIsWord) {
+      word = Arg;
+      nextIsWord = false;
+      continue;
+    }
+
+    if (Arg == "--file") {
+      nextIsWord = false;
+      nextIsFileName = true;
+      continue;
+    }
+    
+    if (Arg == "--word") {
+      nextIsFileName = false;
+      nextIsWord = true;
+      continue;
+    }   
+  }
+
+  std::cout << std::endl << "file: " << fileName << std::endl << "word: " << word;
+
   return 0;
 }
