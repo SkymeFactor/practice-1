0bb92c429a1997cef0bede289db5bf03ee3124b9
Gosha Pogrebitskiy <goshik200712@gmail.com>
2025-09-20 16:35:42 +0300

    added letter counting and comparison, final program ver 1.0

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   bool nextIsWord = false;
   
   std::string fileName = "";
-  std::string word = "";
+  std::string refWord = "";
 
   for (int i = 1; i < argc; i++) { 
     std::string Arg = argv[i];
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
     }
 
     if (nextIsWord) {
-      word = Arg;
+      refWord = Arg;
       nextIsWord = false;
       continue;
     }
@@ -36,8 +36,68 @@ int main(int argc, char** argv) {
       continue;
     }   
   }
+  
+  std::fstream file(fileName);
+  if (!file.is_open()) {
+    std::cout << "Error! Couldn't open file " << fileName << std::endl;
+    return 1;
+  }
 
-  std::cout << std::endl << "file: " << fileName << std::endl << "word: " << word;
+  int refLetCnt[26] = {0}; // the amount of each letter in the reference refWord
+
+  for (int i = 0; i < refWord.size(); i++) {
+    if (refWord[i] >= 'a') {
+      refLetCnt[refWord[i] - 'a']++;
+    }
+    else {
+      refLetCnt[refWord[i] - 'A']++;
+    }
+  }
+
+  // Debugging
+  /*
+  for (int i = 0; i < 26; i++)
+  {
+    if (refLetCnt[i] > 0)
+      std::cout << char('A' + i) << ": " << refLetCnt[i] << std::endl;
+  }
+  */
+ 
+  int count = 0;
+  int letCnt[26];
+
+  while (!file.eof()) {
+    std::string word;
+    file >> word;
+    if (word == "")
+      continue;
+    
+    for (int i = 0; i < 26; i++) {
+      letCnt[i] = 0;
+    }
+
+    for (int i = 0; i < word.size(); i++) {
+      if (word[i] >= 'a') {
+        letCnt[word[i] - 'a']++;
+      }
+      else {
+        letCnt[word[i] - 'A']++;
+      }
+    }
+
+    bool fitsCriteria = true;
+    for (int i = 0; i < 26 && fitsCriteria; i++) {
+      if (letCnt[i] < refLetCnt[i]) {
+        fitsCriteria = false;
+      }
+    }
+
+    if (fitsCriteria) {
+      count++;
+    }
+  }
+
+  std::cout << count;
 
   return 0;
 }
