2877d190ebd4b53c47dde805287736df79239f6f
Vladislav Pivtsaev <vlad.pivtsaev@mail.ru>
2025-09-20 12:15:44 +0300

    Fixed the argument handling and added explanations for errors.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,24 @@
 #include <fstream>
 
 int main(int argc, char** argv) {
-  if (argc == 5 && !(strcmp(argv[1], "--word") && strcmp(argv[3], "--word")) && !(strcmp(argv[1], "--file") && strcmp(argv[3], "--file"))) {
-    char *word = argv[2], *file = argv[4];
-    if (!strcmp(argv[3], "--word")) {
-      word = argv[4], file = argv[2];
+  char *word = argv[0], *file = argv[0];
+  for (int i = 2; i < argc; i++) {
+    if (!strcmp(argv[i - 1], "--word") && !strcmp(word, argv[0])) {
+      word = argv[i];
     }
+    else if (!strcmp(argv[i - 1], "--file") && !strcmp(file, argv[0])) {
+      file = argv[i];
+    }
+  }
+  if (strcmp(word, argv[0]) && strcmp(file, argv[0])) {
     bool word_mask[26] = {};
     for (int i = 0; i < strlen(word); i++) {
       word_mask[word[i] - 'a' + ('a' - 'A') * (word[i] < 'a')] = true;
     }
     std::ifstream in(file);
     if (!in) {
-      std::cout << "Error";
-      return 1;
+      std::cout << "Can't open file";
+      return 2;
     }
     char text_word[1000];
     int res = 0;
@@ -34,7 +39,7 @@ int main(int argc, char** argv) {
     std::cout << res;
   }
   else {
-    std::cout << "Error";
+    std::cout << "Wrong input format";
     return 1;
   }
 }
