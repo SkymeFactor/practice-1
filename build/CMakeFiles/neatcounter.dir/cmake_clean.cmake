58b26ac9dc8a7220075e6fc4ef1515c74e8a423d
X_xp <timurbilalov14@gmail.com>
2025-09-20 17:06:09 +0300

    final

---
+++ build/CMakeFiles/neatcounter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neatcounter.dir/link.d"
+  "CMakeFiles/neatcounter.dir/main.cpp.o"
+  "CMakeFiles/neatcounter.dir/main.cpp.o.d"
+  "neatcounter"
+  "neatcounter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neatcounter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
